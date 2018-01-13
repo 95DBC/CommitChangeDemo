92b16d871b450f99882eb50093502bb5695115ee
95DBC <171291124@qq.com>
2018-01-13 12:53:00 +0800

    写一个Toast 显示出前后的不同

--- app/src/main/java/cn/raymond/commitchangedemo/MainActivity.java
+++ app/src/main/java/cn/raymond/commitchangedemo/MainActivity.java
@@ -2,6 +2,7 @@ package cn.raymond.commitchangedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "CommitChangeTest", Toast.LENGTH_SHORT).show();
     }
 }
