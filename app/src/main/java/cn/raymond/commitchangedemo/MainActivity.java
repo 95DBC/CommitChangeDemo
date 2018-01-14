be1c13ed8a87487473e94a7fe450834fc0420c09
95DBC <171291124@qq.com>
2018-01-14 17:02:12 +0800

    写一个Toast 作为标识第一个分支

--- app/src/main/java/cn/raymond/commitchangedemo/MainActivity.java
+++ app/src/main/java/cn/raymond/commitchangedemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "CommitChangeTest", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Branch1.0.0", Toast.LENGTH_SHORT).show();
     }
 }
